b5f645740b60d47de527d99514d701739c16eaa9
Chirag <chiragtrivedi17@hotmail.com>
2022-10-27 01:27:57 -0400

    1.14

--- p2.2/src/App.js
+++ p2.2/src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react"
-// import "./minimal-table.css"
 
 const Button = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>)
 
@@ -17,9 +16,6 @@ const App = () => {
     "Even the best planning is not so omniscient as to get it right the first time.",
     "How does a project get to be a year late?... One day at a time."
   ]
-  
-  // const [selected, setSelected] = useState(0)
-  // const [voteArr, setVoteArr] = useState([...arr])
 
   const [clicks, setClicks] = useState({
     index: 0,
@@ -27,8 +23,6 @@ const App = () => {
   })
 
   const handleSelected = () => {
-    // console.log("anecdote num " + (clicks.index) + " has " + (clicks.voteArr[clicks.index]) + " votes")
-    
     const newClicks = {
       index: getRandomFunc(5),
       voteArr: [...clicks.voteArr]
@@ -37,7 +31,6 @@ const App = () => {
   }
 
   const handleVote = () => {
-    // console.log("anecdote num " + (clicks.index) + " has " + (clicks.voteArr[clicks.index]) + " votes")
     const newArr = [...clicks.voteArr]
     newArr[clicks.index] += 1    
     
@@ -48,68 +41,24 @@ const App = () => {
     setClicks(newClicks)
   }
 
+  const mostVotesIndex = clicks.voteArr.indexOf(Math.max(...clicks.voteArr)) 
+
   return(
     <div>
+      <h1>Anecdote of the day</h1>
       <font size="4">
         {anecdotes[clicks.index]} <br></br>
         has {clicks.voteArr[clicks.index]} votes <br></br>
       </font>
       <Button handleClick = {handleVote} text = "vote"/>
       <Button handleClick = {handleSelected} text = "next anecdote"/>
+      <h1>Anecdote with most votes</h1>
+      <font size="4">
+        {anecdotes[mostVotesIndex]} <br></br>
+        has {clicks.voteArr[mostVotesIndex]} votes <br></br>
+      </font>
     </div>
   )
 }
 
 export default App
-
-
-
-
-// import { useState } from "react"
-// // import "./minimal-table.css"
-
-// const Button = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>)
-
-// function getRandomFunc(max) {
-//   return Math.floor(Math.random()*(max));
-// }
-
-// var arr = new Array(5).fill(0)
-
-// const App = () => {
-//   const anecdotes =[
-//     "Adding manpower to a late software project makes it later!",
-//     "The best way to get a project done faster is to start sooner",
-//     "Before software can be reusable it first has to be usable.",
-//     "Even the best planning is not so omniscient as to get it right the first time.",
-//     "How does a project get to be a year late?... One day at a time."
-//   ]
-  
-//   const [selected, setSelected] = useState(0)
-//   const [voteArr, setVoteArr] = useState([...arr])
-
-//   const handleSelected = () => {
-//     console.log("anecdote num " + (selected) + " has " + (voteArr[selected]) + " votes")
-//     setSelected(getRandomFunc(5))
-//   }
-
-//   const handleVote = () => {
-//     console.log("anecdote num " + (selected) + " has " + (voteArr[selected]) + " votes")
-//     const newArr = [...voteArr]
-//     newArr[selected] += 1
-//     setVoteArr(newArr)
-//   }
-
-//   return(
-//     <div>
-//       <font size="4">
-//         {anecdotes[selected]} <br></br>
-//         has {voteArr[selected]} votes <br></br>
-//       </font>
-//       <Button handleClick = {handleVote} text = "vote"/>
-//       <Button handleClick = {handleSelected} text = "next anecdote"/>
-//     </div>
-//   )
-// }
-
-// export default App
